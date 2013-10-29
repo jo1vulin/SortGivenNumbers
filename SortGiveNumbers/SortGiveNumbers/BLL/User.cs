f437f3951ffc8a624bdb9ef54214fa984d21bd8b
jo1vulin <jo1vulin@gmail.com>
2013-10-29 11:57:44 +0100

    BLL

---
+++ SortGiveNumbers/SortGiveNumbers/BLL/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SortGiveNumbers.BLL
+{
+    public class User
+    {
+        public User(int UserId, string FirstName, string Lastname)
+        {
+            this.UserID = UserId;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public int UserID { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set; }
+    }
+}
---
+++ SortGiveNumbers/SortGiveNumbers/BLL/UserBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SortGiveNumbers.BLL;
+
+namespace SortGiveNumbers.BLL
+{
+    public class UserBLL
+    {
+        User korisnik = new User(1, "Jovan", "Vulin");
+        User korisnik1 = new User(2, "Milan", "Milic");
+        User korisnik2 = new User(3, "Goran", "Goric");
+        User korisnik3 = new User(4, "Mirko", "Bulic");
+        
+        
+        
+
+        public string getUserNameById(int UserID)
+        {
+            List<User> lista = new List<User> { korisnik, korisnik1, korisnik2, korisnik3 };
+            var userName = from b in lista
+                       where b.UserID == UserID
+                       select b.FirstName.ToString();
+
+
+            return userName.First().ToString();
+        }
+        
+    }
+}
